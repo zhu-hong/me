7de429666b9b48196f0162cc70abcf20205c4f5a
zh <zhxhyy@qq.com>
2023-02-25 00:26:16 +0800

    use netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import unocss from 'unocss/astro'
 import { presetWind } from 'unocss'
 import toc from 'remark-toc'
 import collapse from 'remark-collapse'
+import netlify from '@astrojs/netlify'
 
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     unocss({
       injectReset: false,
@@ -24,4 +27,4 @@ export default defineConfig({
       ],
     ],
   },
-});
+})
